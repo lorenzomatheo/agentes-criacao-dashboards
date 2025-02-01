5ac183c5c3ee383e02fbb23673bed6815f7465fc
Lorenzo <lorenzo.matheo@hotmail.com>
2025-02-01 14:38:48 -0300

    implementacao da funcao de extrair arquivos csv e parte da de qsl

--- dashboardAgent.py
+++ dashboardAgent.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os
 import plotly.express as px
 import dash
@@ -11,17 +12,27 @@ from langchain import *
 import openai
 
 import sqlalchemy
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 import psycopg2
 
+def extract_csv_data(filepath):
+    """Extrai dados de um arquivo CSV."""
+    try:
+        df = pd.read_csv(filepath)
+        return df
+    except Exception as e:
+        print(f"Erro ao extrair CSV: {e}")
+        return None
 
+def extract_sql_data(database_path, query):
+    """Extrai dados de um banco de dados ."""
+    try: 
+        db = session.query(filepath)
+        return db
+    except Exception as e:
+        print(f"Erro ao extrair CSV: {e}")
+        return None
 
-
-
-
-
-
-
-
-
-def extractData():
 
