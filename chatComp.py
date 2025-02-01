d8c96f452c19a3f9b70cbeff5667f98df79dfccb
Lorenzo <lorenzo.matheo@hotmail.com>
2025-02-01 11:19:31 -0300

    importacao do modelo OpenAI 3.5 turbo, importacao das bibliotecas necessárias e início da funcao de extrair dados

---
+++ chatComp.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+client = OpenAI()
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Vou ter uma reunião. Quero que faça um dashboard."},
+  ]
+)
---
+++ dashboardAgent.py
@@ -0,0 +1,27 @@
+import os
+import plotly.express as px
+import dash
+from dash import dcc, html
+import requests
+import os
+import json
+from datetime import datetime
+
+from langchain import * 
+import openai
+
+import sqlalchemy
+import psycopg2
+
+
+
+
+
+
+
+
+
+
+
+def extractData():
+
