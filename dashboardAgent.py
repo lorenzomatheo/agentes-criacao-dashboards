16be8061399b03fa2bad2ee79a1d69d436147113
Lorenzo <lorenzo.matheo@hotmail.com>
2025-02-03 16:29:42 -0300

    retirada funcao extract_sql_data(), substuicao importacao bibliotecas LangChain e criacao do Agente SQL, importcao da API key e da llm, alteracao da instancia do SQLDataBase

--- dashboardAgent.py
+++ dashboardAgent.py
@@ -8,7 +8,14 @@ import os
 import json
 from datetime import datetime
 
-from langchain import * 
+from langchain.llms import openai
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.agents import create_sql_agent
+from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain_openai import ChatOpenAI
+from langchain.agents import AgentExecutor
 import openai
 
 import sqlalchemy
@@ -16,6 +23,14 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 import logging
+
+
+
+openai_api_key = os.getenv['OPENAI_API_KEY']
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
 
 
 def extract_csv_data(filepath):
@@ -49,18 +64,55 @@ def create_session(engine):
         return None
 
 
-def extract_sql_data(session, query):
-    """Extrai dados de um banco de dados SQL usando SQLAlchemy e session."""
-    try: 
-        result = session.execute(text(query))
-        df = pd.DataFrame(result.fetchall(), columns=result.keys())
-        logging.info(f"Dados extraídos com sucesso do banco de dados. Query: {query}")
-        return df
-    except SQLAlchemyError as e:
-        logging.error(f"Erro ao extrair dados do banco de dados: {e}")
+def create_sql_agent_from_session(session,engine):
+    """ Criação de Agente SQL com LangChain a partir da session do SQLALchemy"""
+    if session is None:
+        logging.error("Sessão SQLAlchemy inválida para criar o agente SQL")
         return None
+    
+    try:
+        # Cria uma instância do SQLDataBase a partir da engine
+        
+        db = SQLDatabase(engine=engine)
+        
+        # Cria a toolkit
+        
+        toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+        # Cria o agente executor
+        agent_executor = create_sql_agent(
+            llm=llm,
+            toolkit=toolkit,
+            verbose=True
+        )
+
+        logging.info("Agente SQL criado com sucesso!")
+        
+        return agent_executor
+    
     except Exception as e:
-        logging.error(f"Erro inesperado ao extrair dados: {e}")
+        logging.error(f"Erro ao criar Agente SQL: {e}")
         return None
+
+def query_with_langchain(agent_executor, user_query):
+
+
+
+
+
+
+
+
+    # PENDENTE
+    # def extract_json_data():
+    #    pass
+
+    #PENDENTE
+    # def padronize_date_columns(df):
+    #    pass
+
+    #PENDENTE
+    # def convert_type_column(df):
+    #    pass
 
 
